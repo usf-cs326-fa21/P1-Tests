669e2b21322ebf4b92d2e061058f5d5f413dd928
Matthew Malensek <matt@malensek.net>
2021-09-20 14:51:02 -0700

    Add human readable units test

--- 12-Human-Readable-Units-1.c
+++ 12-Human-Readable-Units-1.c
@@ -13,79 +13,38 @@
 #include "procfs.h"
 #include "util.h"
 
-test_start("Tests uid_to_uname function.");
+test_start("Tests human_readable_size function.");
 
-subtest("Comparing output against known username (root)",
+subtest("Comparing output against known values",
 {
-    char name[16] = { 'X' };
-    uid_to_uname(name, 0);
-    test_assert_str(name, "==", "root", 16);
-    test_printf("'%s'", name);
-});
+    char buf[128];
+    human_readable_size(buf, 128, 0, 1);
+    test_assert_str(buf, "==", "0.0 KiB", 500);
+    test_printf("%s\n", buf);
 
-subtest("Mapping an unknown UID should yield the same UID in string form",
-{
-    char *uid_file = build_path(getenv("TEST_DIR"), "/inputs/all_uids");
+    human_readable_size(buf, 128, 1023, 1);
+    test_assert_str(buf, "==", "1023.0 KiB", 500);
+    test_printf("%s\n", buf);
 
-    /* First, we need to find a UID that is *NOT* valid on this system. Luckily
-     * we have sorted the list of UIDs so this is a bit easier... */
+    human_readable_size(buf, 128, 1024, 8);
+    test_assert_str(buf, "==", "1.00000000 MiB", 500);
+    test_printf("%s\n", buf);
 
-    FILE *fp = fopen(uid_file, "r");
-    test_assert(fp != NULL);
+    human_readable_size(buf, 128, 34359738367, 1);
+    test_assert_str(buf, "==", "32.0 TiB", 500);
+    test_printf("%s\n", buf);
 
-    int tested = 0;
-    uid_t uid = 0;
-    char line[128];
-    while (fgets(line, 128, fp) && tested < 3) {
-        if (atoi(line) != uid) {
-            printf("Using UID: %d\n", uid);
+    human_readable_size(buf, 128, 2844524, 1);
+    test_assert_str(buf, "==", "2.7 GiB", 500);
+    test_printf("%s\n", buf);
 
-            char name[16] = { 'X' };
-            uid_to_uname(name, uid);
+    human_readable_size(buf, 128, 472877960873902080, 1);
+    test_assert_str(buf, "==", "420.0 ZiB", 500);
+    test_printf("%s\n", buf);
 
-            char same[16];
-            snprintf(same, 16, "%d", uid);
-
-            test_assert_str(name, "==", same, 16);
-            test_printf("'%s'", name);
-
-            tested++;
-        }
-
-        uid++;
-    }
-    fclose(fp);
-
-});
-
-subtest("Comparing output against getpwuid(3). Also tests username length truncation.",
-{
-    char *uid_file = build_path(getenv("TEST_DIR"), "/inputs/all_uids");
-    FILE *fp = fopen(uid_file, "r");
-    test_assert(fp != NULL);
-
-    uid_t uid = 0;
-    char line[128];
-    while (fgets(line, 128, fp)) {
-        uid = atoi(line);
-
-        /* We'll use getpwuid(3) as our reference function: */
-        struct passwd *pw = getpwuid(uid);
-        test_assert(pw != NULL);
-
-        char expected_username[16];
-        strncpy(expected_username, pw->pw_name, 16);
-        expected_username[15] = '\0';
-
-        char name[16] = { 'X' };
-        uid_to_uname(name, uid);
-
-        test_assert_str(name, "==", expected_username, 16);
-        test_printf("'%s'", expected_username);
-        test_printf("'%s'", name);
-    }
-
-    fclose(fp);
+    human_readable_size(buf, 128, 129049729231119, 2);
+    test_assert_str(buf, "==", "117.37 PiB", 500);
+    test_printf("%s\n", buf);
 });
 
 test_end
